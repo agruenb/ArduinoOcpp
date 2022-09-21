99b600a1a6feb7dbbeb916835b39878915932772
Adrian Grünberg <adriang2000@gmx.de>
2022-09-21 12:00:04 +0200

    adjusted test files to naming convention

---
+++ test/unitTests/unitTestsMain.cpp
@@ -0,0 +1,6 @@
+// Compile implementation of Catch for use with files that do contain tests:
+// - g++ -std=c++11 -Wall -I$(CATCH_SINGLE_INCLUDE) -c 000-CatchMain.cpp
+// - cl -EHsc -I%CATCH_SINGLE_INCLUDE% -c 000-CatchMain.cpp
+
+#define CATCH_CONFIG_MAIN
+#include "../catch2/catch.hpp"
